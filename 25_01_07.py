a7502114e0c2dd16b06c8e2ddcb5752b6b08e37d
hjchoi <juhye987@naver.com>
2025-01-07 20:54:01 +0900

    [BAEJOON]senven_dwarfs

--- 25_01_07.py
+++ 25_01_07.py
@@ -1,3 +1,4 @@
+# Using itertools.ver
 import itertools
 
 dwarf = [int(input()) for _ in range(9)]
@@ -13,3 +14,24 @@ for i in list(comb):
         a.sort()
         print(*a, sep='\n')
         break
+
+# Not Using itertools.ver
+def find_del_value(del_value):
+    for i in range(len(dwarf)):
+        for j in range(len(dwarf)):
+            if i == j:
+                continue
+            if (dwarf[i]+dwarf[j]) == del_value:
+                del_one = dwarf[i]
+                del_two = dwarf[j]
+                return del_one, del_two
+
+if __name__=='__main__':
+    dwarf = [int(input()) for _ in range(9)]
+    all_sum = sum(dwarf)
+    del_value = all_sum - 100
+    value_,value__= find_del_value(del_value)
+    dwarf.remove(value_)
+    dwarf.remove(value__)
+    dwarf.sort()
+    print(*dwarf, sep='\n')
