5605ce10d909f1ed25f487752326dd368078e318
hjchoi <juhye987@naver.com>
2025-01-29 00:45:14 +0900

    [BAEJOON] 11726

--- 25_01_30.py
+++ 25_01_30.py
@@ -1,8 +1,9 @@
 n = int(input())
-dp = [0]*(n+1)
-dp[1]=1
-dp[2]=2
-
-for i in range(3,n+1):
-    dp[i] = (dp[i-1]+dp[i-2])%10007
-print(dp[n])
+dp = [0] * (n + 1)
+if n < 3:
+	print(n)
+else:
+    dp[1], dp[2] = 1, 2
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 10007
+    print(dp[n])
